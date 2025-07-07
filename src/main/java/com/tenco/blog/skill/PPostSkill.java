d5e117947bb445dfce503f6956e399732214fea5
coramdeo643 <coramdeo643@kakao.com>
2025-07-07 10:12:54 +0900

    skill

---
+++ src/main/java/com/tenco/blog/skill/PPostSkill.java
@@ -0,0 +1,26 @@
+package com.tenco.blog.skill;
+
+import com.tenco.blog.ppost.PPost;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "ppost_skill_tb")
+@Entity
+public class PPostSkill {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ppost_id")
+	private PPost ppost;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "skill_id")
+	private Skill skill;
+
+}
