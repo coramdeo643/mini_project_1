37b25d3508c88e5d460e7f29e2f9199c1b66c332
jang seungwon <rhanwnf97@naver.com>
2025-07-07 14:33:56 +0900

    지원현황

--- src/main/java/com/tenco/blog/application/ApplicationJpaRepository.java
+++ src/main/java/com/tenco/blog/application/ApplicationJpaRepository.java
@@ -1,12 +1,68 @@
 package com.tenco.blog.application;
 
+import com.tenco.blog.board.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 public interface ApplicationJpaRepository extends JpaRepository<Application,Long> {
 
-    // 회사가 지원자의 합격/불합격 통보 기능을 status 의 값을 변경해주면서 알려줄 예정
+//     회사가 지원자의 합격/불합격 통보 기능을 status 의 값을 변경해주면서 알려줄 예정
 
-    // 중복 확인 체크할 예정
+//     공고 ID 로 한번에 유저 정보도 가져오기 JOIN FETCH 사용하면 됨
+// 회사가 등록한 공고에 대한 모든 지원자를 가져오기
+
+       @Query("SELECT b FROM Board b JOIN FETCH b.company c ORDER BY b.id DESC")
+       List<Board> findAllJoinUser();
+
+       // 회사가 사용하는거
+//       @Query("SELECT a FROM Application a JOIN FETCH a.user u WHERE a.board.id = :boardId")
+//       List<Application> findAllByBoardIdWithUser(@Param("boardId") Long boardId);
+
+       // 회사 테스트
+       @Query("SELECT a FROM Application a " +
+               "JOIN FETCH a.user u " +
+               "JOIN FETCH a.board b " +
+               "WHERE b.company.id = :companyId")
+       List<Application> findAllByBoardIdWithUser(@Param("companyId") Long companyId);
+       // 유저가 사용하는거
+       @Query("SELECT a FROM Application a JOIN FETCH a.board b WHERE a.user.id = :userId")
+       List<Application> findAllByUserIdWithBoard(@Param("userId") Long userId);
+
+//       // 1 단계 모든회사의 모든 지원자 보기
+//       @Query("SELECT a FROM Application a " +
+//               "JOIN FETCH a.board b " +
+//               "JOIN FETCH a.user u " +
+//               "WHERE b.company.id = :companyId")
+//       List<Application> findAllByCompanyId(@Param("companyId") Long companyId);
+
+       // 2 단계 회사의 모든 공고 보기
+       @Query("SELECT a FROM Application a " +
+               "JOIN FETCH a.board b " +
+               "JOIN FETCH a.user u " +
+               "JOIN FETCH a.company c " +
+               "WHERE b.company.id = :companyId")
+       List<Application> findAllByCompanyId(@Param("companyId") Long companyId);
+
+
+
+
+       // 3 . 2단계의 쿼리문과 비교 점이 먼지 알아보고 사용할 것
+       @Query("SELECT a FROM Application a JOIN FETCH a.board b JOIN FETCH b.company JOIN FETCH a.user u")
+       List<Application> findAllJoinUserAndBoardAndCompany();
+
+
+
+
+       // 유저 입장에서 자기가 지원한 공고들 조회
+       @Query("SELECT a FROM Application a JOIN FETCH a.board b JOIN FETCH b.company c WHERE a.user.id = :userId")
+       List<Application> findAllByUserIdWithBoardAndCompany(@Param("userId") Long userId);
+
+
+
 }
---
+++ src/main/java/com/tenco/blog/application/ApplicationService.java
@@ -0,0 +1,81 @@
+package com.tenco.blog.application;
+
+import com.tenco.blog._core.errors.exception.Exception404;
+import com.tenco.blog.board.Board;
+import com.tenco.blog.board.BoardJpaRepository;
+import com.tenco.blog.company.Company;
+import com.tenco.blog.company.CompanyRepository;
+import com.tenco.blog.user.User;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ApplicationService {
+
+    // 양방향 이기에 여러 레파지토리를 가져올 것이다
+    private static final Logger log = LoggerFactory.getLogger(ApplicationService.class);
+    private final ApplicationJpaRepository applicationJpaRepository;
+    private final BoardJpaRepository boardJpaRepository;
+    private final CompanyRepository companyRepository;
+
+
+
+    // 지원 기능
+    @Transactional
+    public void save(ApplicationRequest.SaveDTO saveDTO, User sessionUser) {
+        log.info("공고에 지원하는 서비스 처리 시작 - 공고 ID {} , 지원자 {} ",
+                saveDTO.getBoardId(),sessionUser.getUsername());
+
+        // 지원시 공고가 존재하는지 존재 여부 확인
+        Board board = boardJpaRepository.findById(saveDTO.getBoardId())
+                .orElseThrow(() -> new Exception404("존재하지 않는 공고임으로 지원 불가"));
+        Company company = companyRepository.findById(saveDTO.getCompanyId())
+                .orElseThrow(() -> new Exception404("존재하지 않는 회사 입니다."));
+
+        // 준 영속상태
+        Application application = saveDTO.toEntity(sessionUser,board,company);
+
+        // 저장 - 정방향 insert 처리
+        applicationJpaRepository.save(application);
+    }
+
+
+    public void deleteById(Long applicationId, User sessionUser) {
+        log.info("지원 취소 서비스 처리 시작 - 댓글 ID{}");
+
+        Application application = applicationJpaRepository.findById(applicationId)
+                .orElseThrow(() -> new Exception404("지원한 이력이 없습니다."));
+
+
+        applicationJpaRepository.deleteById(applicationId);
+    }
+
+    // 회사가 사용하는 지원자 확인
+    public List<Application> findAllByBoardIdWithUser(Long companyId) {
+        return applicationJpaRepository.findAllByBoardIdWithUser(companyId);
+    }
+
+    // 유저가 사용하는 공고 확인
+    public List<Application> findAllByBoardIdWithBoard(Long userId) {
+        return applicationJpaRepository.findAllByUserIdWithBoard(userId);
+    }
+
+
+
+
+    @Transactional
+    public void updateStatus(Long applicationId, String status) {
+        Application application = applicationJpaRepository.findById(applicationId)
+                .orElseThrow(() -> new Exception404("해당 지원서를 찾을 수 없습니다."));
+        application.setStatus(status); //
+    }
+
+
+}
